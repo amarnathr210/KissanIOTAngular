ad29c9a36aae1e6d57f4f3404e599b61b37c4c72
amarnathr210 <amarnathr210@gmail.com>
2019-12-11 22:15:06 +0530

    removed the commented code

--- src/app/admin-lte.conf.ts
+++ src/app/admin-lte.conf.ts
@@ -4,7 +4,7 @@ export const adminLteConf = {
   // isSidebarLeftExpandOnOver: false,
   // isSidebarLeftMouseOver: false,
   // isSidebarLeftMini: true,
-  // sidebarRightSkin: 'dark',
+  // sidebarRightSkin: "green",
   // isSidebarRightCollapsed: true,
   // isSidebarRightOverContent: true,
   // layout: 'normal',
@@ -24,40 +24,5 @@ export const adminLteConf = {
     },
     { label: "Add Device", route: "device", iconClasses: "fa fa-tasks" },
     { label: "Add Locations", route: "location", iconClasses: "fa fa-tasks" }
-    // {
-    //   label: "Layout",
-    //   iconClasses: "fa fa-th-list",
-    //   children: [
-    //     { label: "Configuration", route: "layout/configuration" },
-    //     { label: "Custom", route: "layout/custom" },
-    //     { label: "Header", route: "layout/header" },
-    //     { label: "Sidebar Left", route: "layout/sidebar-left" },
-    //     { label: "Sidebar Right", route: "layout/sidebar-right" },
-    //     { label: "Content", route: "layout/content" }
-    //   ]
-    // },
-    // { label: "COMPONENTS", separator: true },
-    // { label: "Accordion", route: "accordion", iconClasses: "fa fa-tasks" },
-    // {
-    //   label: "Alert",
-    //   route: "alert",
-    //   iconClasses: "fa fa-exclamation-triangle"
-    // },
-    // {
-    //   label: "Boxs",
-    //   iconClasses: "fa fa-files-o",
-    //   children: [
-    //     { label: "Default Box", route: "boxs/box" },
-    //     { label: "Info Box", route: "boxs/info-box" },
-    //     { label: "Small Box", route: "boxs/small-box" }
-    //   ]
-    // },
-    // { label: "Dropdown", route: "dropdown", iconClasses: "fa fa-arrows-v" },
-    // {
-    //   label: "Form",
-    //   iconClasses: "fa fa-files-o",
-    //   children: [{ label: "Input Text", route: "form/input-text" }]
-    // },
-    // { label: "Tabs", route: "tabs", iconClasses: "fa fa-th" }
   ]
 };
